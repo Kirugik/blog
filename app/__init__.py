c5b47fdcf6f81b9bb10d1d8426f1bcd71b9e9ca1
Robert Kirui <kirugik79@gmail.com>
2022-05-14 16:31:47 +0300

    Feat: Create database and initialize it with flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,55 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+from os import path 
+from flask_login import LoginManager 
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = "sksht52393nsy23"
+
+    # tell flask the location of the database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     
-    
+    # initializing the database with flask app
+    db.init_app(app) 
+
+
     from .views import views 
+    from .auth import auth 
 
     app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/") 
 
 
-    return app  
+    from.models import User, Post, Comment, Like   
+
+    create_database(app)
+
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id)) 
+
+
+    return app 
+
+
+# creating the database
+def create_database(app):
+    # first, check if the database exists
+    if not path.exists("website/" + DB_NAME):
+        # if it does not exist, create one 
+        db.create_all(app=app)
+        print("Created database")
