01136a9961e6cf465aca368699c8a988f764fc61
Robert Kirui <kirugik79@gmail.com>
2022-05-14 16:14:47 +0300

    Feat: Create blog posts, comments, and likes view functions

--- app/views.py
+++ app/views.py
@@ -44,7 +44,93 @@ def delete_post(id):
     elif current_user.id != post.id:
         flash("You cannot delete this post.", category='error')
     else:
-        db.session.delete(post)
+        db.session.delete(post) 
         db.session.commit()
         flash("Post deleted", category='success')
+    return redirect(url_for('views.home')) 
+
+
+
+@views.route("/posts/<username>")
+@login_required   
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        flash("No user with that username.", category='error')
+        return redirect(url_for('views.home')) 
+    posts = user.posts 
+    return render_template('posts.html', user=current_user, posts=posts, username=username) 
+
+
+
+@views.route("/create-comment/<post_id>", methods=['POST']) 
+@login_required   
+def create_comment(post_id):
+    text = request.form.get('text') 
+
+    # check if a comment has been typed in the comment input
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    # if there is a comment typed ...
+    else:
+        # make sure there is a post for which the comment is being made 
+        post = Post.query.filter_by(id=post_id)
+        # if there is a post ...
+        if post:
+            # creating a new comment and saving to db
+            comment = Comment(text=text, author=current_user.id, post_id=post_id) 
+            db.session.add(comment)
+            db.session.commit()
+
+        # if there is no post ...
+        else:
+            flash('Post does not exist.', category='error')
+
+
+    return redirect(url_for('views.home'))
+
+
+
+
+@views.route("/delete-comment/<comment_id>") 
+@login_required   
+def delete_comment(comment_id):
+    # Check if the comment exists in the db
+    comment = Comment.query.filter_by(id=comment_id).first()
+    # if there is no comment...
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    # if there is a comment, check if the current user(the logged in user) is the author of either the post or the comment
+    # we only want the comment to be deleted by either the author of the post or author of the comment
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You are not allowed to delete this comment.', category='error') 
+    # if there is comment and the current user(logged in user) is either the author of the post or author of the comment, delete the comment from db 
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+        flash('Comment deleted!', category='success')
+    return redirect(url_for('views.home')) 
+
+
+
+@views.route("/like-post/<post_id>", methods=['GET'])  
+@login_required   
+def like(post_id):
+    # Check if there is a post and a like in the db
+    post = Post.query.filter_by(id=post_id)
+    like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
+
+    # if there is no post...
+    if not post:
+        flash('Post does not exist.', category='error')
+    # if there is a post with a like, remove the like
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    # if there is a post with or without a like, add a like 
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like) 
+        db.session.commit()
     return redirect(url_for('views.home')) 
